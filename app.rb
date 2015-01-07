1f3a0ec57f66b9633c04ba54ade8214bc8b338f7
Debbie Cravey & Mike Goren <student@epicodus.com>
2015-01-07 10:58:11 -0800

    add Sinatra files

---
+++ app.rb
@@ -0,0 +1,15 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/rock_paper_scissors')
+
+get('/form') do
+  erb(:form)
+end
+
+get('/results') do
+  input1 = params.fetch('input1')
+  input2 = params.fetch('input2')
+  @winner_string = input1.determine_winner(input2)
+  erb(:results)
+end
